fed08ce1d2034673453d9dd7084b88ac119cc52d
suvadip_gosh <suvadip.gosh@mheducation.com>
2022-10-17 20:22:06 +0530

    map polyfill

---
+++ map/index.js
@@ -0,0 +1,20 @@
+const radius = [5, 3, 2, 8];
+
+function area(radius) {
+  return radius * radius;
+}
+
+Array.prototype.calculate = function (logic) {
+  const output = [];
+
+  for(let i = 0; i < this.length; i++) {
+    output.push(logic(this[i]));
+  }
+
+  return output;
+}
+
+
+const res = radius.calculate(area);
+
+console.log(res);
