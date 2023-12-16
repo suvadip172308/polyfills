3c3fb9775d395d42af9057e87ad904fadc36d96b
suvadip_gosh <suvadip.gosh@mheducation.com>
2023-12-16 14:24:59 +0530

    polyfill apply

---
+++ apply/index.js
@@ -0,0 +1,31 @@
+const person = {
+  firstName: 'Suvadip',
+  lastName: 'Ghosh',
+};
+
+const printName = function(hometown, state) {
+  console.log(this.firstName + ' ' + this.lastName + ' from ' + hometown + ',' + state);
+}
+
+// Polyfill of "call"
+
+Function.prototype.myCall = function(context, ...args) {
+  currentContext  = context || globalThis;  // take provided context else glabal context
+  let randomProp = Math.random();   // create a random number as prop
+
+  /** Check the random prop is already exist in current context object */
+  while(currentContext[randomProp] !== undefined) { 
+    randomProp = Math.random();
+  }
+
+  currentContext[randomProp] = this;  // store 'this' to the randomProp
+
+  const result = currentContext[randomProp](...args); // call all other parameters on current context
+
+  delete currentContext[randomProp]; //delete the random prop created by us
+
+  return result;
+}
+
+printName.call(person, 'chinsurah', 'west bengal');
+printName.myCall(person, 'chinsurah', 'west bengal');
